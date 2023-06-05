2dd73119cea7c92f2d9c011c75bbf22cec1a4fb6
Colleague Riley <ColleagueRiley@gmail.com>
2023-06-05 18:32:59 -0400

    more progress made towards cleaning the generated syntax + made code slightly more smooth

--- main.c
+++ main.c
@@ -7,77 +7,79 @@
 
 #include <assert.h>
 
+/* shortened function names */
+#define strAppend si_string_append
+#define strAppendL si_string_append_len
+
 
 siString* print_token(stb_lexer *lexer, siString* c_code) {
   switch (lexer->token) {
-    case CLEX_id        : si_string_append(c_code, lexer->string); break;
-    case CLEX_eq        : si_string_append(c_code, "=="); break;
-    case CLEX_noteq     : si_string_append(c_code, "!="); break;
-    case CLEX_lesseq    : si_string_append(c_code, "<="); break;
-    case CLEX_greatereq : si_string_append(c_code, ">="); break;
-    case CLEX_andand    : si_string_append(c_code, "&&"); break;
-    case CLEX_oror      : si_string_append(c_code, "||"); break;
-    case CLEX_shl       : si_string_append(c_code, "<<"); break;
-    case CLEX_shr       : si_string_append(c_code, ">>"); break;
-    case CLEX_plusplus  : si_string_append(c_code, "++"); break;
-    case CLEX_minusminus: si_string_append(c_code, "--"); break;
-    case CLEX_arrow     : si_string_append(c_code, "->"); break;
-    case CLEX_andeq     : si_string_append(c_code, "&="); break;
-    case CLEX_oreq      : si_string_append(c_code, "|="); break;
-    case CLEX_xoreq     : si_string_append(c_code, "^="); break;
-    case CLEX_pluseq    : si_string_append(c_code, "+="); break;
-    case CLEX_minuseq   : si_string_append(c_code, "-="); break;
-    case CLEX_muleq     : si_string_append(c_code, "*="); break;
-    case CLEX_diveq     : si_string_append(c_code, "/="); break;
-    case CLEX_modeq     : si_string_append(c_code, "%%="); break;
-    case CLEX_shleq     : si_string_append(c_code, "<<="); break;
-    case CLEX_shreq     : si_string_append(c_code, ">>="); break;
-    case CLEX_eqarrow   : si_string_append(c_code, "=>"); break;
+    case CLEX_id        : strAppend(c_code, lexer->string); break;
+    case CLEX_eq        : strAppendL(c_code, "==", 2); break;
+    case CLEX_noteq     : strAppendL(c_code, "!=", 2); break;
+    case CLEX_lesseq    : strAppendL(c_code, "<=", 2); break;
+    case CLEX_greatereq : strAppendL(c_code, ">=", 2); break;
+    case CLEX_andand    : strAppendL(c_code, "&&", 2); break;
+    case CLEX_oror      : strAppendL(c_code, "||", 2); break;
+    case CLEX_shl       : strAppendL(c_code, "<<", 2); break;
+    case CLEX_shr       : strAppendL(c_code, ">>", 2); break;
+    case CLEX_plusplus  : strAppendL(c_code, "++", 2); break;
+    case CLEX_minusminus: strAppendL(c_code, "--", 2); break;
+    case CLEX_arrow     : strAppendL(c_code, "->", 2); break;
+    case CLEX_andeq     : strAppendL(c_code, "&=", 2); break;
+    case CLEX_oreq      : strAppendL(c_code, "|=", 2); break;
+    case CLEX_xoreq     : strAppendL(c_code, "^=", 2); break;
+    case CLEX_pluseq    : strAppendL(c_code, "+=", 2); break;
+    case CLEX_minuseq   : strAppendL(c_code, "-=", 2); break;
+    case CLEX_muleq     : strAppendL(c_code, "*=", 2); break;
+    case CLEX_diveq     : strAppendL(c_code, "/=", 2); break;
+    case CLEX_modeq     : strAppendL(c_code, "%%=", 2); break;
+    case CLEX_shleq     : strAppendL(c_code, "<<=", 3); break;
+    case CLEX_shreq     : strAppendL(c_code, ">>=", 3); break;
+    case CLEX_eqarrow   : strAppendL(c_code, "=>", 2); break;
     case CLEX_dqstring  : 
-          si_string_append(c_code, "\""); 
-          si_string_append(c_code, lexer->string); 
-          si_string_append(c_code, "\""); 
-          
+          strAppendL(c_code, "\"", 1); 
+          strAppendL(c_code, lexer->string, lexer->string_len); 
+          strAppendL(c_code, "\"", 1); 
           break;
     case CLEX_sqstring  : 
-          si_string_append(c_code, "\"'"); 
-          si_string_append(c_code, lexer->string); 
-          si_string_append(c_code, "\"'"); 
+          strAppendL(c_code, "\"'", 2); 
+          strAppendL(c_code, lexer->string, lexer->string_len); 
+          strAppendL(c_code, "\"'", 2); 
     case CLEX_charlit   : 
-          si_string_append(c_code, "'"); 
-          si_string_append(c_code, lexer->string); 
-          si_string_append(c_code, "'"); 
+          strAppendL(c_code, "'", 1); 
+          strAppendL(c_code, lexer->string, lexer->string_len); 
+          strAppendL(c_code, "'", 1); 
     #if defined(STB__clex_int_as_double) && !defined(STB__CLEX_use_stdlib)
-    case CLEX_intlit    : si_string_append(c_code, "#%g", lexer->real_number); break;
+    case CLEX_intlit    : strAppend(c_code, "#%g", lexer->real_number); break;
     #else
     case CLEX_intlit    : 
-      //si_string_append(c_code, lexer->int_number); break;
+      //strAppend(c_code, lexer->int_number); break;
     #endif
     case CLEX_floatlit  : 
-      //si_string_append(c_code, lexer->real_number); break;
+      //strAppend(c_code, lexer->real_number); break;
     default:
         if (lexer->token >= 0 && lexer->token < 256) {
           switch(lexer->token) {
             case '{':
-                si_string_append(c_code, &lexer->token);
-                si_string_append(c_code, "\n");
+                strAppendL(c_code, &lexer->token, 1);
+                strAppendL(c_code, "\n", 1);
                 break;
             
             case '}':
-              si_string_append(c_code, &lexer->token);
-              si_string_append(c_code, "\n\n");
+              strAppendL(c_code, &lexer->token, 1);
+              strAppendL(c_code, "\n\n", 2);
               break;
 
             case ';':
-                if (si_string_back(c_code) == '\n')
-                  si_string_erase(c_code, si_string_len(c_code) - 1, si_string_len(c_code));
+                si_string_erase(c_code, si_string_len(c_code) - 1, si_string_len(c_code));
 
-                si_string_append(c_code, &lexer->token);
-                si_string_append(c_code, "\n");
+                strAppendL(c_code, &lexer->token, 1);
+                strAppendL(c_code, "\n", 1);
                 break;
             
             default:
-              si_string_append(c_code, &lexer->token);
+              strAppendL(c_code, &lexer->token, 1);
               break;
           }
         }
@@ -86,12 +88,19 @@ siString* print_token(stb_lexer *lexer, siString* c_code) {
         
         break;
   }
+  
+  if (c_code[si_string_len(c_code) - 1] != si_string_back(c_code))
+    printf("hi\n");
 
-  si_string_append(c_code, " ");
+  if (c_code[si_string_len(c_code) - 1] != '\n') {
+    strAppendL(c_code, " ", 1);
+  }
+  else
+    printf("hi\n");
 }
 
 int main(int argc, char **argv) {
-  siFile f = si_file_open_mode("test.cp", SI_FILE_MODE_READ);
+  siFile f = si_file_open("test.cp");
   siString text = si_file_read(f);
   si_file_close(f);
 
